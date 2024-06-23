60c923ef6bb022cee1e918d3b74cc9d2b9cf213c
daysikroyter <nubchenko@gmail.com>
2024-06-23 21:16:50 +0300

    added section projects & rates

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,9 @@
 const swiper = new Swiper('.projects__slider', {
+  spaceBetween: 20,
+  effect: "coverflow",
+  coverflowEffect: {
+    slideShadows: false
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
